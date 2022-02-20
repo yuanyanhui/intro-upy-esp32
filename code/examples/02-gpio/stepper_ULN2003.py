16982dd1cbff8f2f38b56c5aa7d3bc9ea4a0bbff
yuanyanhui <learnforfun@gmail.com>
2022-02-20 19:01:04 +0800

    cleanup

--- code/examples/02-gpio/stepper_ULN2003.py
+++ code/examples/02-gpio/stepper_ULN2003.py
@@ -12,18 +12,18 @@ IN3 = Pin(5,  Pin.OUT)
 IN4 = Pin(18, Pin.OUT)
 
 # half-step mode
-# clockwise step sequence
-seq_cw = [[1, 0, 0, 0],
-          [1, 1, 0, 0],
-          [0, 1, 0, 0],
-          [0, 1, 1, 0],
-          [0, 0, 1, 0],
-          [0, 0, 1, 1],
-          [0, 0, 0, 1],
-          [1, 0, 0, 1]]
+# counter clockwise step sequence
+seq_ccw = [[1, 0, 0, 0],
+           [1, 1, 0, 0],
+           [0, 1, 0, 0],
+           [0, 1, 1, 0],
+           [0, 0, 1, 0],
+           [0, 0, 1, 1],
+           [0, 0, 0, 1],
+           [1, 0, 0, 1]]
 
 # counterclockwise step sequence
-seq_ccw = seq_cw[::-1]
+seq_cw = seq_ccw[::-1]
 
 delay = 1  # ms, delay between steps
 
@@ -35,7 +35,7 @@ for i in range(4096):
     IN3.value(seq_cw[step][2])
     IN4.value(seq_cw[step][3])
     sleep_ms(1)
-    
+
 # one counterclockwise revolution (4096 steps)
 for i in range(4096):
     step = i % 8
--- code/examples/06-i2c/mpu6050.py
+++ code/examples/06-i2c/mpu6050.py
@@ -66,22 +66,23 @@ ACCEL_CONFIG = 0x1C
 GYRO_CONFIG = 0x1B
 MPU_CONFIG = 0x1A
 
+
 class MPU6050:
     def __init__(self, i2c, address=0x68):
         self.i2c = i2c
         self.address = address
         # gyro offsets
-        self.bx = 0.0   
+        self.bx = 0.0
         self.by = 0.0
         self.bz = 0.0
         # for complementary filter
-        self.phi_hat = 0.0  
-        self.theta_hat = 0.0  
-        
+        self.phi_hat = 0.0
+        self.theta_hat = 0.0
+
         # default accelerometer and gyro configuration
         self.accel_scale_modifier = ACCEL_SCALE_MODIFIER_2G
         self.gyro_scale_modifier = GYRO_SCALE_MODIFIER_250DEG
-        
+
         # Wake up the MPU-6050 since it starts in sleep mode
         self.i2c.writeto_mem(self.address, PWR_MGMT_1, bytearray([0x00]))
 
@@ -94,13 +95,14 @@ class MPU6050:
         # MSB (15-8) are read first followed by LSB (7-0) (see mpu6050 Register Map)
         raw_bytes = self.i2c.readfrom_mem(self.address, register, 2)
         return ustruct.unpack('>h', raw_bytes)[0]  # big endian, short
-        
+
     def set_sample_rate(self, sample_rate):
         """
         set sample rate
         """
         # Write the new range to the SMPLRT_DIV register
-        self.i2c.writeto_mem(self.address, SMPLRT_DIV, bytearray([sample_rate]))
+        self.i2c.writeto_mem(self.address, SMPLRT_DIV,
+                             bytearray([sample_rate]))
 
     # MPU-6050 Methods
     def get_temp(self):
@@ -133,9 +135,10 @@ class MPU6050:
             self.accel_scale_modifier = ACCEL_SCALE_MODIFIER_2G
 
         # Write the new range to the ACCEL_CONFIG register
-        self.i2c.writeto_mem(self.address, ACCEL_CONFIG, bytearray([accel_range]))
+        self.i2c.writeto_mem(self.address, ACCEL_CONFIG,
+                             bytearray([accel_range]))
 
-    def get_accel(self, g = True):
+    def get_accel(self, g=True):
         """
         Gets and returns the X, Y and Z values from the accelerometer.
         If g is True, it will return the data in g
@@ -157,14 +160,16 @@ class MPU6050:
             y = y * GRAVITIY_MS2
             z = z * GRAVITIY_MS2
             return x, y, z
-            
+
     # deg
     def get_accel_angles(self):
         ax, ay, az = self.get_accel()
         # roll - axis x (deg)
-        phi = math.atan2(ay, math.sqrt(ax ** 2.0 + az ** 2.0))* 180.0 / math.pi
+        phi = math.atan2(ay, math.sqrt(
+            ax ** 2.0 + az ** 2.0)) * 180.0 / math.pi
         # pitch - axis y (deg)
-        theta = math.atan2(-ax, math.sqrt(ay ** 2.0 + az ** 2.0)) * 180.0 / math.pi
+        theta = math.atan2(-ax, math.sqrt(ay ** 2.0 +
+                           az ** 2.0)) * 180.0 / math.pi
 
         return phi, theta
 
@@ -183,21 +188,25 @@ class MPU6050:
         elif gyro_range == GYRO_RANGE_2000DEG:
             self.gyro_scale_modifier = GYRO_SCALE_MODIFIER_2000DEG
         else:
-            print("Unkown range - gyro_scale_modifier set to self.GYRO_SCALE_MODIFIER_250DEG")
+            print(
+                "Unkown range - gyro_scale_modifier set to self.GYRO_SCALE_MODIFIER_250DEG")
             self.gyro_scale_modifier = GYRO_SCALE_MODIFIER_250DEG
-        
+
         # First change it to 0x00 to make sure we write the correct value later
         self.i2c.writeto_mem(self.address, GYRO_CONFIG,  bytearray([0x00]))
 
         # Write the new range to the ACCEL_CONFIG register
-        self.i2c.writeto_mem(self.address, GYRO_CONFIG,  bytearray([gyro_range]))
+        self.i2c.writeto_mem(self.address, GYRO_CONFIG,
+                             bytearray([gyro_range]))
 
-    def set_filter_range(self, filter_range = FILTER_BW_256):
+    def set_filter_range(self, filter_range=FILTER_BW_256):
         """Sets the low-pass bandpass filter frequency"""
         # Keep the current EXT_SYNC_SET configuration in bits 3, 4, 5 in the MPU_CONFIG register
         EXT_SYNC_SET_bytes = self.i2c.readfrom_mem(self.address, MPU_CONFIG, 1)
-        EXT_SYNC_SET = int.from_bytes(EXT_SYNC_SET_bytes, 'little') & 0b00111000
-        self.i2c.writeto_mem(self.address, MPU_CONFIG, bytearray([EXT_SYNC_SET | filter_range]))
+        EXT_SYNC_SET = int.from_bytes(
+            EXT_SYNC_SET_bytes, 'little') & 0b00111000
+        self.i2c.writeto_mem(self.address, MPU_CONFIG,
+                             bytearray([EXT_SYNC_SET | filter_range]))
 
     def get_gyro(self):
         """Gets and returns the X, Y and Z values from the gyroscope.
@@ -206,20 +215,20 @@ class MPU6050:
         x = self.read_i2c_word(GYRO_XOUT0)
         y = self.read_i2c_word(GYRO_YOUT0)
         z = self.read_i2c_word(GYRO_ZOUT0)
-        
+
         # dps (deg/s)
         x = x / self.gyro_scale_modifier
         y = y / self.gyro_scale_modifier
         z = z / self.gyro_scale_modifier
 
         return x, y, z
-        
+
     # get gyro offsets, deg/s
     def calibrate(self, N=100):
         bx = 0.0
         by = 0.0
         bz = 0.0
-        print("Calibration starts... Do not move sensor!")
+        print("Calibration started... Do not move sensor!")
         for i in range(N):
             gx, gy, gz = self.get_gyro()
             bx += gx
@@ -227,7 +236,7 @@ class MPU6050:
             bz += gz
             sleep_ms(10)
         print("Calibration done...")
-        self.bx, self.by, self.bz = bx/N, by/N, bz/N 
+        self.bx, self.by, self.bz = bx/N, by/N, bz/N
 
     def get_all_data(self):
         """Reads and returns all the available data."""
@@ -236,7 +245,7 @@ class MPU6050:
         gyro = self.get_gyro_data()
 
         return accel, gyro, temp
-    
+
     # complementary_filter
     # phi_hat, theta_hat (deg)
     def get_angle_xy(self):
@@ -249,10 +258,10 @@ class MPU6050:
 
         dt = time() - start_time
         start_time = time()
-        
+
         # Get estimated angles from raw accelerometer data
         phi_hat_acc, theta_hat_acc = self.get_accel_angles()
-        
+
         # Get raw gyro data and subtract biases (deg/s)
         p, q, r = self.get_gyro()
         p -= self.bx
@@ -262,14 +271,14 @@ class MPU6050:
         # Calculate Euler angle derivatives (deg/s)
         # phi_dot = p + math.sin(phi_hat * math.pi / 180.0) * math.tan(theta_hat* math.pi / 180.0) * q + math.cos(phi_hat* math.pi / 180.0) * math.tan(theta_hat* math.pi / 180.0) * r
         # theta_dot = math.cos(phi_hat* math.pi / 180.0) * q - math.sin(phi_hat* math.pi / 180.0) * r
-        
+
         phi_dot = p
         theta_dot = q
-        
-        # Update complimentary filter (unit: deg)
-        self.phi_hat = (1 - alpha) * (self.phi_hat + dt * phi_dot) + alpha * phi_hat_acc
-        self.theta_hat = (1 - alpha) * (self.theta_hat + dt * theta_dot) + alpha * theta_hat_acc   
-        
-        return self.phi_hat, self.theta_hat   # roll-x axis, pitch-y axis
-    
 
+        # Update complimentary filter (unit: deg)
+        self.phi_hat = (1 - alpha) * (self.phi_hat +
+                                      dt * phi_dot) + alpha * phi_hat_acc
+        self.theta_hat = (1 - alpha) * (self.theta_hat +
+                                        dt * theta_dot) + alpha * theta_hat_acc
+
+        return self.phi_hat, self.theta_hat   # roll-x axis, pitch-y axis
